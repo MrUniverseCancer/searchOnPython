8c855eebc573e13eb2d1e3dfa153dc1900c6b88e
MrUniverseCancer <huyangjia@mail.ustc.edu.cn>
2024-02-19 17:51:08 +0800

    wrong try

--- Fourier_expansion/image.py
+++ Fourier_expansion/image.py
@@ -1,5 +1,8 @@
 import head
+import numpy as np
 from head import cv
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 def cv_show(img,name):
@@ -11,28 +14,108 @@ def temp(img, a, b):
     v = cv.Canny(img, a, b)
     cv_show(v,'img')
 
+def r_cal(x, y, center):
+    fact_x = x - center[0]
+    fact_y = y - center[1]
+    return np.sqrt( fact_x**2 + fact_y**2 )
+
+def theta_cal(x, y ,center):
+    fact_x = x - center[0]
+    fact_y = y - center[1]
+    return np.arctan2(fact_y,fact_x)
+
+def GetNew(sum,number):
+    if sum == 10 
+
+# def ani_Make():
+#     # 创建一个大小为10x6的图形
+#     fig, ax = plt.subplots(figsize=(10, 6))
+
+#     # 初始化数据
+#     data = np.random.rand(100, 100)
+
+#     # 在图形中创建一个imshow对象
+#     im = ax.imshow(data, cmap='gray')
+    
+#     # 定义一个更新函数，每次调用会更新数据集并更新imshow对象
+#     def update(frame):
+#         data = np.random.rand(100, 100)  # 更新数据集
+#         im.set_array(data)  # 更新imshow对象的数据
+#         return im,
+    
+#     # 创建一个动画对象，每隔50毫秒调用一次更新函数
+#     ani = FuncAnimation(fig, update, frames=100, interval=50)
+
+#     # 显示动画
+#     plt.show()
+
+
 if __name__ == '__main__':
     
     img=cv.imread('./test_figure.jpg',cv.IMREAD_GRAYSCALE)
     v1 = cv.Canny(img, 80, 150)
     # v2 = cv.Canny(img, 50, 100)
-    # res = head.np.hstack((v1,v2))
+    # res = np.hstack((v1,v2))
     # cv_show(res,'res')
     res = v1
     x = res.shape[0]
     y = res.shape[1]
-    fact = head.np.zeros((x,y))
+    fact = np.zeros((x,y))
     center = (x//2, y//2)
     i = 0
     j = 0
-    while i < x :
-        while j < y:
-            if v1[i][j] != 0:
-                fact[i][j] = 1
-            j += 1
-        i += 1
-        j = 0
+    ############################
+    # Polar_r = []
+    # Polar_theta = []
+    # while i < x :
+    #     while j < y:
+    #         if v1[i][j] != 0:
+    #             fact[i][j] = 1
+    #             Polar_r.append(r_cal(i,j,center))
+    #             Polar_theta.append(theta_cal(i,j,center))
+    #         j += 1
+    #     i += 1
+    #     j = 0
+    ########################
+    (place_x, place_y) = np.nonzero(res)
+    Polar_r = r_cal(place_x, place_y, center)
+    Polar_theta = theta_cal(place_x, place_y, center)
+    valid_length = place_x.size
+    # ani_Make()
+
+
+    # 创建一个大小为10x6的图形
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # 初始化数据
+    data = np.random.rand(x, y)
     
-    Polar_r = head.np.sqrt((-center[0])**2 + (-center[1])**2)
-    Polar_theta = head.np.arctan2(y - center[1], x - center[0])
+    # 在图形中创建一个imshow对象
+    im = ax.imshow(data, cmap='gray')
+    data = np.zeros((x,y)) + 1
+    sum = 0 #Make begin check
+    number = 0
+    # fact = np.zeros((x,y)) + 1
+    # fact[50:100,50:100] = 0
+    # 定义一个更新函数，每次调用会更新数据集并更新imshow对象
+    def update(frame):
+        global data 
+        global sum
+        global number
+        result = GetNew(sum, number) #添加的像素点
+        if sum < 10 :
+            data = np.zeros((x,y)) + 1
+            sum += 1
+        elif number < valid_length :
+            data[place_x[number]][place_y[number]] = 0 #原始图像
+            result *= data #最终结果
+            number += 1
+        im.set_array(result)  # 更新imshow对象的数据
+        return im,
+
+    # 创建一个动画对象，每隔50毫秒调用一次更新函数
+    ani = FuncAnimation(fig, update, frames=1000, interval=1)
+
+    # 显示动画
+    plt.show()
     
--- Fourier_expansion/temp.py
+++ Fourier_expansion/temp.py
@@ -1,60 +1,121 @@
-# print("Hello World")
-# def temp(i):
-#     print(i)
+# # print("Hello World")
+# # def temp(i):
+# #     print(i)
 
-# temp(100)
-import cv2
+# # temp(100)
+# import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+# import matplotlib.animation as animation
+# import image
+
+# _image = cv2.imread("./test_figure.jpg", cv2.IMREAD_GRAYSCALE)
+# contours, _ = cv2.findContours(_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# # image.cv_show(_image, "tmep")
+
+# # 提取第一个轮廓
+# contour = contours[0][:, 0, :]
+
+# # 对轮廓进行傅立叶变换
+# contour_complex = np.empty(contour.shape[:-1], dtype=complex)
+# contour_complex.real = contour[:, 0]
+# contour_complex.imag = contour[:, 1]
+
+# # 进行傅立叶变换
+# contour_fft = np.fft.fft(contour_complex)
+
+
+# # 设置动画参数
+# num_frames = 100  # 动画帧数
+# max_radius = 200  # 最大半径
+# angles = np.linspace(0, 2 * np.pi, num_frames)  # 在0到2π之间均匀分布的角度
+
+# # 生成圆周运动轨迹
+# trajectory = np.zeros((num_frames, len(contour), 2), dtype=float)
+# for i in range(num_frames):
+#     for j in range(len(contour)):
+#         radius = np.abs(contour_fft[j])  # 使用傅里叶变换结果确定半径
+#         phase = np.angle(contour_fft[j])  # 使用傅里叶变换结果确定相位
+#         x = contour[j, 0] + radius * np.cos(phase + angles[i])  # x 坐标
+#         y = contour[j, 1] + radius * np.sin(phase + angles[i])  # y 坐标
+#         trajectory[i, j] = [x, y]
+
+# # 绘制轨迹动画
+# fig, ax = plt.subplots()
+# ax.set_xlim(0, _image.shape[1])
+# ax.set_ylim(0, _image.shape[0])
+
+# line, = ax.plot([], [], 'r-')
+
+# def init():
+#     line.set_data([], [])
+#     return line,
+
+# def update(frame):
+#     line.set_data(trajectory[frame, :, 0], trajectory[frame, :, 1])
+#     return line,
+
+# ani = animation.FuncAnimation(fig, update, frames=num_frames, init_func=init, blit=True)
+# plt.show()
+
+# str_temp = "Hello"
+# change = ord('A') - ord('a')
+# for ichar in str_temp:
+#     if ichar > 'A' and ichar < 'Z':
+#         ichar = chr(ord(ichar) - change)
+# else:
+#     print(str_temp)  
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.animation import FuncAnimation
+
+# # 创建一个空图形
+# fig, ax = plt.subplots(figsize=(10, 6))
+
+# # 在图形中创建一个空曲线
+# line, = ax.plot([], [])
+# ax.set_ylim(-1.2, 1.2)
+# ax.set_xlim(-1,10)
+# # 定义一个更新函数，每次调用会更新曲线的数据
+# def update(frame):
+#     x = np.linspace(0, 0.5*np.pi, 50) 
+#     temp = np.linspace(1.5*np.pi, 2*np.pi,50)
+#     x = np.hstack((x,temp))
+#     y = np.sin(x + frame/10.0)
+#     line.set_data(x, y)
+#     return line,
+
+# # 创建一个动画对象，每隔50毫秒调用一次更新函数
+# ani = FuncAnimation(fig, update, interval=50)
+
+# # 显示动画
+# plt.show()
+
+
 import numpy as np
-from matplotlib import pyplot as plt
-import matplotlib.animation as animation
-import image
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-_image = cv2.imread("./test_figure.jpg", cv2.IMREAD_GRAYSCALE)
-contours, _ = cv2.findContours(_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# 创建一个大小为10x6的图形
+fig, ax = plt.subplots(figsize=(10, 6))
 
-# image.cv_show(_image, "tmep")
+# 初始化数据
+data = np.random.rand(100, 100)
 
-# 提取第一个轮廓
-contour = contours[0][:, 0, :]
+# 在图形中创建一个imshow对象
+im = ax.imshow(data, cmap='gray')
 
-# 对轮廓进行傅立叶变换
-contour_complex = np.empty(contour.shape[:-1], dtype=complex)
-contour_complex.real = contour[:, 0]
-contour_complex.imag = contour[:, 1]
-
-# 进行傅立叶变换
-contour_fft = np.fft.fft(contour_complex)
-
-
-# 设置动画参数
-num_frames = 100  # 动画帧数
-max_radius = 200  # 最大半径
-angles = np.linspace(0, 2 * np.pi, num_frames)  # 在0到2π之间均匀分布的角度
-
-# 生成圆周运动轨迹
-trajectory = np.zeros((num_frames, len(contour), 2), dtype=float)
-for i in range(num_frames):
-    for j in range(len(contour)):
-        radius = np.abs(contour_fft[j])  # 使用傅里叶变换结果确定半径
-        phase = np.angle(contour_fft[j])  # 使用傅里叶变换结果确定相位
-        x = contour[j, 0] + radius * np.cos(phase + angles[i])  # x 坐标
-        y = contour[j, 1] + radius * np.sin(phase + angles[i])  # y 坐标
-        trajectory[i, j] = [x, y]
-
-# 绘制轨迹动画
-fig, ax = plt.subplots()
-ax.set_xlim(0, _image.shape[1])
-ax.set_ylim(0, _image.shape[0])
-
-line, = ax.plot([], [], 'r-')
-
-def init():
-    line.set_data([], [])
-    return line,
-
+# 定义一个更新函数，每次调用会更新数据集并更新imshow对象
 def update(frame):
-    line.set_data(trajectory[frame, :, 0], trajectory[frame, :, 1])
-    return line,
+    # data = np.random.rand(100, 100)  # 更新数据集
+    data = np.zeros((100,100)) + 255 
+    im.set_array(data)  # 更新imshow对象的数据
+    return im,
 
-ani = animation.FuncAnimation(fig, update, frames=num_frames, init_func=init, blit=True)
+# 创建一个动画对象，每隔50毫秒调用一次更新函数
+ani = FuncAnimation(fig, update, frames=100, interval=50)
+
+# 显示动画
 plt.show()
